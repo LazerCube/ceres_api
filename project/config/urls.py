db7732c3276673541549dff85a0ae50fe4904ff2
Elliot <elliot@lazercube.com>
2016-07-23 21:12:42 +0100

    Temp fix for unsecure management views

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,8 +1,29 @@
-from django.conf.urls import include, url
+import os
+
+from django.conf.urls import include, url, patterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+from oauth2_provider import views
+from django.conf import settings
+
+"""
+Include oauth2_provider urls one by one because "/applications" urls are not secure.
+See this issue :  https://github.com/evonove/django-oauth-toolkit/issues/196
+Waitting for fix in the meantime
+"""
+print(os.environ.get('DJANGO_SETTINGS_MODULE'))
+
 urlpatterns = [
     url(r'^', include('authentication.urls')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^authorize/$', views.AuthorizationView.as_view(), name="authorize"),
+    url(r'^token/$', views.TokenView.as_view(), name="token"),
+    url(r'^revoke_token/$', views.RevokeTokenView.as_view(), name="revoke-token"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Warning : the following lines are unsecure
+if settings.DEBUG:
+    # Application management views
+    urlpatterns += [
+        url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    ]
